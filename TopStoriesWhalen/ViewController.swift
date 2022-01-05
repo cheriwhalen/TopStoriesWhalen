9569364c66d9dd8625fd5831e18822a0153cc2f1
cheriwhalen <33527247+cheriwhalen@users.noreply.github.com>
2022-01-05 13:49:02 -0600

    Initial Commit

---
+++ TopStoriesWhalen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TopStoriesWhalen
+//
+//  Created by Cheri Whalen on 1/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
